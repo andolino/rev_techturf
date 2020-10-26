87b4d8727de90340e9a62a55973c1fefc12335d5
andolino <dondonpentavia@gmail.com>
2020-10-26 23:52:16 +0800

    2020-10-27

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
-@section('content')
+@extends('index')
+<!-- @section('content')
 	<div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
 		<h1 class="text-dark display-4">Todo App</h1>
 		<todo-component></todo-component>
 	</div>
-@endsection
+@endsection -->
